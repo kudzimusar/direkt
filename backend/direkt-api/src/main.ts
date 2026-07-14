af2b9099da952d3dafdbafc0fee4c15708e64cae
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-14 23:44:28 +0900

    feat(backend): add API bootstrap entry point

---
+++ backend/direkt-api/src/main.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { configureApplication } from './configure-application';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  configureApplication(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('PORT');
+  await app.listen(port, '0.0.0.0');
+}
+
+void bootstrap();
