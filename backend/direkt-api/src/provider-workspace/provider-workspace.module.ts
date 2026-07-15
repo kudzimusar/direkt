40f38a6dd0c149d6a59457fdd012133084ec48ac
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-15 23:59:24 +0900

    feat: register provider workspace commands

--- backend/direkt-api/src/provider-workspace/provider-workspace.module.ts
+++ backend/direkt-api/src/provider-workspace/provider-workspace.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProviderModule } from '../provider-core/provider.module';
+import { ProviderWorkspaceCommandRepository } from './provider-workspace-command.repository';
 import { ProviderWorkspaceController } from './provider-workspace.controller';
 import { ProviderWorkspaceRepository } from './provider-workspace.repository';
 import { ProviderWorkspaceService } from './provider-workspace.service';
@@ -7,6 +8,10 @@ import { ProviderWorkspaceService } from './provider-workspace.service';
 @Module({
   imports: [ProviderModule],
   controllers: [ProviderWorkspaceController],
-  providers: [ProviderWorkspaceRepository, ProviderWorkspaceService],
+  providers: [
+    ProviderWorkspaceCommandRepository,
+    ProviderWorkspaceRepository,
+    ProviderWorkspaceService,
+  ],
 })
 export class ProviderWorkspaceModule {}
