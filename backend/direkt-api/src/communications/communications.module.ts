99b4dcdbcbc426e3aec0bc03182cfb6c5a17a7cb
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-22 11:56:43 +0900

    feat(rc4): wire FCM communications services

--- backend/direkt-api/src/communications/communications.module.ts
+++ backend/direkt-api/src/communications/communications.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DisabledEmailProviderAdapter } from './disabled-email-provider.adapter';
+import { DisabledPushProviderAdapter } from './disabled-push-provider.adapter';
 import { EMAIL_PROVIDER } from './email-provider.port';
 import { EmailOutboxService } from './email-outbox.service';
+import { FcmPushProviderAdapter } from './fcm-push-provider.adapter';
+import { PUSH_PROVIDER } from './push-provider.port';
+import { PushDeviceController } from './push-device.controller';
+import { PushDeviceTokenService } from './push-device-token.service';
+import { PushOutboxService } from './push-outbox.service';
 import { ResendEmailProviderAdapter } from './resend-email-provider.adapter';
 
 @Module({
+  controllers: [PushDeviceController],
   providers: [
     {
       provide: EMAIL_PROVIDER,
@@ -21,8 +28,24 @@ import { ResendEmailProviderAdapter } from './resend-email-provider.adapter';
         );
       },
     },
+    {
+      provide: PUSH_PROVIDER,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const mode = configService.getOrThrow<string>('PUSH_PROVIDER_MODE');
+        if (mode === 'disabled') {
+          return new DisabledPushProviderAdapter();
+        }
+        return new FcmPushProviderAdapter(
+          configService.getOrThrow<string>('FIREBASE_PROJECT_ID'),
+          configService.getOrThrow<number>('PUSH_REQUEST_TIMEOUT_MS'),
+        );
+      },
+    },
     EmailOutboxService,
+    PushDeviceTokenService,
+    PushOutboxService,
   ],
-  exports: [EmailOutboxService],
+  exports: [EmailOutboxService, PushDeviceTokenService, PushOutboxService],
 })
 export class CommunicationsModule {}
