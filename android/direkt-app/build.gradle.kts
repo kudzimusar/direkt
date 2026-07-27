443b0dfe127028cc9e7577c0c312c087ca51ffb5
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-07-27 21:10:37 +0900

    feat(rc9b): enable Kotlin serialization plugin

--- android/direkt-app/build.gradle.kts
+++ android/direkt-app/build.gradle.kts
@@ -9,6 +9,7 @@ import org.gradle.api.tasks.TaskAction
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.crashlytics) apply false
 }
